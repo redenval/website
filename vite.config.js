21640f89e82cc142a5415002e5953f6289997b04
redenval <105330870+redenval@users.noreply.github.com>
2022-12-02 21:42:02 +0800

    deploy git

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { build } from 'vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		outDir: './build'
+	}
 };
 
 export default config;
